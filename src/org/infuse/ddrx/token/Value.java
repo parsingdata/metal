371f5683506809d4f09393a659e4fba8d10488ad
Jeroen van den Bos <jeroen@infuse.org>
2014-01-02 15:45:00 +0100

    Added Type as argument to Value.

--- src/org/infuse/ddrx/token/Value.java
+++ src/org/infuse/ddrx/token/Value.java
@@ -18,22 +18,32 @@ package org.infuse.ddrx.token;
 
 import org.infuse.ddrx.expression.Expression;
 import org.infuse.ddrx.expression.Predicate;
+import org.infuse.ddrx.expression.Type;
 
 public class Value implements Token {
     
     private final String _name;
+    private final Type _type;
     private final Expression _size;
     private final Predicate _pred;
     
-    public Value(String name, Expression size, Predicate pred) {
+    public Value(String name, Type type, Expression size, Predicate pred) {
         _name = name;
+        _type = type;
         _size = size;
         _pred = pred;
     }
 
     @Override
     public boolean eval() {
-        // TODO Auto-generated method stub
+        // Evaluate size.
+        // Read size from stream.
+        // Validate type.
+        // Determine if stored predicates can be evaluated.
+        // If so, evaluate stored predicates and return false if one fails.
+        // Determine if predicate can be evaluated.
+        // If so, evaluate and return result.
+        // If not, store predicate.
         return false;
     }
     
