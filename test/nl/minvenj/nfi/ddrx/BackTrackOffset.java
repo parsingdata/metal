dc9cb06f7a8540928257680814f04164f3b5c72a
Jeroen van den Bos <jeroen@infuse.org>
2014-02-12 09:55:29 +0100

    Backtracking tests for repeat.

--- test/nl/minvenj/nfi/ddrx/BackTrackOffset.java
+++ test/nl/minvenj/nfi/ddrx/BackTrackOffset.java
@@ -18,44 +18,82 @@ package nl.minvenj.nfi.ddrx;
 
 import java.math.BigInteger;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 import nl.minvenj.nfi.ddrx.expression.True;
 import nl.minvenj.nfi.ddrx.expression.comparison.Equals;
 import nl.minvenj.nfi.ddrx.expression.value.Con;
 import nl.minvenj.nfi.ddrx.expression.value.Ref;
 import nl.minvenj.nfi.ddrx.io.ByteStream;
 import nl.minvenj.nfi.ddrx.token.Choice;
+import nl.minvenj.nfi.ddrx.token.Repeat;
 import nl.minvenj.nfi.ddrx.token.Sequence;
 import nl.minvenj.nfi.ddrx.token.Token;
 import nl.minvenj.nfi.ddrx.token.Value;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 @RunWith(JUnit4.class)
 public class BackTrackOffset {
-	
-	private Token _backTrack = new Choice(
-			new Sequence(anyValue("a"), fixedValue("b", 2)),
-			new Sequence(anyValue("c"), fixedValue("d", 3)));
-	
-	private Token anyValue(String name) {
-		return new Value(name, new Con(BigInteger.valueOf(1)), new True());
-	}
-	
-	private Token fixedValue(String name, int value) {
-		return new Value(name, new Con(BigInteger.valueOf(1)), new Equals(new Ref(name), new Con(BigInteger.valueOf(value))));
-	}
-	
-	@Test
-	public void directMatchOffset1() {
-		Assert.assertTrue(_backTrack.eval(new ByteStream(new byte[] { 1, 2 })));
-	}
-	
-	@Test
-	public void backTrackOffset1() {
-		Assert.assertTrue(_backTrack.eval(new ByteStream(new byte[] { 1, 3 })));
-	}
-	
+
+    private Token _backTrackChoice = new Choice(
+                                                new Sequence(any("a"), fixed("b", 2)),
+                                                new Sequence(any("c"), fixed("d", 3)));
+
+    private Token _backTrackRepeat = new Sequence(
+                                                  new Repeat(new Sequence(fixed("a", 1), fixed("b", 2))),
+                                                  new Sequence(fixed("c", 1), fixed("d", 3)));
+
+    private Token any(String name) {
+        return new Value(name, new Con(BigInteger.valueOf(1)), new True());
+    }
+
+    private Token fixed(String name, int value) {
+        return new Value(name, new Con(BigInteger.valueOf(1)), new Equals(new Ref(name), new Con(BigInteger.valueOf(value))));
+    }
+    
+    private ByteStream stream(int... bytes) {
+        byte[] out = new byte[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            out[i] = (byte)bytes[i];
+        }
+        return new ByteStream(out);
+    }
+
+    @Test
+    public void choiceLeft() {
+        Assert.assertTrue(_backTrackChoice.eval(stream(1, 2)));
+    }
+
+    @Test
+    public void choiceRight() {
+        Assert.assertTrue(_backTrackChoice.eval(stream(1, 3)));
+    }
+    
+    @Test
+    public void choiceNone() {
+        Assert.assertFalse(_backTrackChoice.eval(stream(1, 4)));
+    }
+    
+    @Test
+    public void repeatZero() {
+        Assert.assertTrue(_backTrackRepeat.eval(stream(1, 3)));
+    }
+
+    @Test
+    public void repeatOnce() {
+        Assert.assertTrue(_backTrackRepeat.eval(stream(1, 2, 1, 3)));
+    }
+
+    @Test
+    public void repeatTwice() {
+        Assert.assertTrue(_backTrackRepeat.eval(stream(1, 2, 1, 2, 1, 3)));
+    }
+
+    @Test
+    public void repeatNone() {
+        Assert.assertFalse(_backTrackRepeat.eval(stream(1, 4)));
+    }
+
 }
