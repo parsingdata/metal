aa81b8602bdedba8b8993022e0bfab722be71c2c
Jeroen van den Bos <jeroen@infuse.org>
2014-03-06 09:12:32 +0100

    Added CRC32 validation to PNG description.

--- test/nl/minvenj/nfi/ddrx/PNG.java
+++ test/nl/minvenj/nfi/ddrx/PNG.java
@@ -16,6 +16,7 @@
 
 package nl.minvenj.nfi.ddrx;
 
+import static nl.minvenj.nfi.ddrx.Shorthand.cat;
 import static nl.minvenj.nfi.ddrx.Shorthand.con;
 import static nl.minvenj.nfi.ddrx.Shorthand.defNum;
 import static nl.minvenj.nfi.ddrx.Shorthand.defVal;
@@ -23,34 +24,52 @@ import static nl.minvenj.nfi.ddrx.Shorthand.eq;
 import static nl.minvenj.nfi.ddrx.Shorthand.expTrue;
 import static nl.minvenj.nfi.ddrx.Shorthand.not;
 import static nl.minvenj.nfi.ddrx.Shorthand.refNum;
+import static nl.minvenj.nfi.ddrx.Shorthand.refVal;
 import static nl.minvenj.nfi.ddrx.Shorthand.rep;
 import static nl.minvenj.nfi.ddrx.Shorthand.seq;
 import static nl.minvenj.nfi.ddrx.data.Environment.stream;
 
 import java.io.IOException;
-
-import nl.minvenj.nfi.ddrx.token.Token;
+import java.math.BigInteger;
+import java.util.zip.CRC32;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import nl.minvenj.nfi.ddrx.data.Environment;
+import nl.minvenj.nfi.ddrx.expression.value.NumericValue;
+import nl.minvenj.nfi.ddrx.expression.value.UnaryValueExpression;
+import nl.minvenj.nfi.ddrx.expression.value.Value;
+import nl.minvenj.nfi.ddrx.expression.value.ValueOperation;
+import nl.minvenj.nfi.ddrx.token.Token;
+
 @RunWith(JUnit4.class)
 public class PNG {
     
     private static final String FILE = "testdata/test.png";
     
-    private static final Token PNG_HEADER = seq(defVal("header1", con(1), eq(con(0x89))),
-                                                seq(defVal("header2", con(3), eq(con("PNG"))),
-                                                    defVal("header3", con(4), eq(con(0x0d0a1a0a)))));
+    private static final Token PNG_HEADER = seq(defVal("highbit", con(1), eq(con(0x89))),
+                                                seq(defVal("PNG", con(3), eq(con("PNG"))),
+                                                    defVal("controlchars", con(4), eq(con(0x0d0a1a0a)))));
     private static final Token PNG_FOOTER = seq(defNum("footerlength", con(4), eq(con(0))),
                                                 seq(defVal("footertype", con(4), eq(con("IEND"))),
                                                     defVal("footercrc32", con(4), eq(con(0xae426082)))));
     private static final Token PNG_STRUCT = seq(defNum("length", con(4), expTrue()),
                                                 seq(defVal("chunktype", con(4), not(eq(con("IEND")))),
                                                     seq(defVal("chunkdata", refNum("length"), expTrue()),
-                                                        defVal("crc32", con(4), expTrue()))));
+                                                        defVal("crc32", con(4), eq(new UnaryValueExpression<Value>(cat(refVal("chunktype"), refVal("chunkdata"))) {
+                                                            @Override
+                                                            public Value eval(Environment env) {
+                                                                return _op.eval(env).operation(new ValueOperation() {
+                                                                    @Override
+                                                                    public Value execute(byte[] value) {
+                                                                        CRC32 crc = new CRC32();
+                                                                        crc.update(value);
+                                                                        return new NumericValue(BigInteger.valueOf(crc.getValue()));
+                                                                    }});
+                                                            }})))));
     private static final Token PNG = seq(PNG_HEADER,
                                          seq(rep(PNG_STRUCT),
                                              PNG_FOOTER));
