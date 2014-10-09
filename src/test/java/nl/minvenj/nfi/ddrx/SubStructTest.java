cc3e902fef3f9e6a4c4757b53ffc3e142ebcc600
Jeroen van den Bos <jeroen@infuse.org>
2014-10-09 08:29:36 +0200

    Refactored Substruct unit tests.

--- src/test/java/nl/minvenj/nfi/ddrx/SubStructTest.java
+++ src/test/java/nl/minvenj/nfi/ddrx/SubStructTest.java
@@ -28,8 +28,9 @@ import static nl.minvenj.nfi.ddrx.util.EnvironmentFactory.stream;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 
+import nl.minvenj.nfi.ddrx.data.Environment;
 import nl.minvenj.nfi.ddrx.data.ParseResult;
 import nl.minvenj.nfi.ddrx.data.ParsedValueList;
 import nl.minvenj.nfi.ddrx.token.Token;
@@ -37,44 +38,52 @@ import nl.minvenj.nfi.ddrx.token.Token;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(JUnit4.class)
+@RunWith(Parameterized.class)
 public class SubStructTest {
     
-    @Test
-    public void offsetTable() throws IOException {
-        ParseResult res = rep(sub(any("a"))).parse(stream(1, 3, 9, 5), enc());
-        Assert.assertTrue(res.succeeded());
-        traverse(5, res.getEnvironment().order);
-    }
+    private final Token _token;
+    private final Environment _env;
+    private final boolean _result;
+    private final int[] _values;
+    private final int[] _offsets;
     
-    private void traverse(int next, ParsedValueList list) {
-        if (next == 0) {
-            Assert.assertNull(list);
-        } else {
-            Assert.assertEquals(list.head.asNumeric().intValue(), next);
-            traverse((int)list.head.getOffset(), list.tail);
-        }
-    }
-    
-    @Test
-    public void linkedList() throws IOException {
-        Token t = sub(seq(def("header", con(1), eq(con(0))),
-                          def("next", con(1)),
-                          def("footer", con(1), eq(con(1)))), "next");
-        ParseResult res = rep(t).parse(stream(0, 8, 1, 42, 0, 12, 1, 84, 0, 4, 1), enc());
+    @Parameters(name="{0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "unnamed", rep(sub(any("a"))), stream(1, 3, 9, 5), true, new int[] { 1, 3, 5 }, new int[] { 0, 1, 3 }},
+                { "named", rep(sub(seq(def("header", con(1), eq(con(0))),
+                                       def("next", con(1)),
+                                       def("footer", con(1), eq(con(1)))), "next")),
+                                       stream(0, 8, 1, 42, 0, 12, 1, 84, 0, 4, 1), true, new int[] { 0, 8, 1, 0, 4, 1, 0, 12, 1 }, new int[] { 0, 1, 2, 8, 9, 10, 4, 5, 6 }}
                                    /* offset: 0, 1, 2,  3, 4,  5, 6,  7, 8, 9,10
                                     * struct: -------      --------      -------
                                     * ref 1:     +-----------------------^
                                     * ref 2:               ^----------------+
                                     * ref 3:                   +----------------*
                                     */
-        Assert.assertTrue(res.succeeded());
-        List<Integer> values = Arrays.asList(0, 8, 1, 0, 4, 1, 0, 12, 1);
+        });
+    }
+    
+    public SubStructTest(final String desc, final Token token, final Environment env, final boolean result, final int[] values, final int[] offsets) {
+        _token = token;
+        _env = env;
+        _result = result;
+        _values = values;
+        _offsets = offsets;
+    }
+    
+    @Test
+    public void test() throws IOException {
+        final ParseResult res = _token.parse(_env, enc());
+        Assert.assertEquals(_result, res.succeeded());
+        Assert.assertEquals(_values.length, _offsets.length);
         ParsedValueList order = res.getEnvironment().order.reverse();
-        for (int i = 0; i < values.size(); i++) {
-            Assert.assertEquals((int)values.get(i), order.head.asNumeric().intValue());
+        for (int i = 0; i < _values.length; i++) {
+            Assert.assertEquals(_values[i], order.head.asNumeric().intValue());
+            Assert.assertEquals(_offsets[i], order.head.getOffset());
             order = order.tail;
         }
         Assert.assertNull(order);
