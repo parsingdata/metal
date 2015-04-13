5813aa036b524c60987714c4fa40e2388345e59c
Jeroen van den Bos <jeroen@infuse.org>
2015-04-13 13:13:26 +0200

    METAL-13: Excercise the null checks in unit tests.

--- src/test/java/nl/minvenj/nfi/metal/ArgumentsTest.java
+++ src/test/java/nl/minvenj/nfi/metal/ArgumentsTest.java
@@ -18,6 +18,7 @@ package nl.minvenj.nfi.metal;
 
 import static nl.minvenj.nfi.metal.Shorthand.con;
 
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -30,6 +31,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import nl.minvenj.nfi.metal.data.Environment;
+import nl.minvenj.nfi.metal.data.ParseResult;
 import nl.minvenj.nfi.metal.encoding.Encoding;
 import nl.minvenj.nfi.metal.expression.Expression;
 import nl.minvenj.nfi.metal.expression.comparison.Eq;
@@ -44,6 +46,17 @@ import nl.minvenj.nfi.metal.expression.value.arithmetic.Neg;
 import nl.minvenj.nfi.metal.expression.value.reference.First;
 import nl.minvenj.nfi.metal.expression.value.reference.Offset;
 import nl.minvenj.nfi.metal.expression.value.reference.Ref;
+import nl.minvenj.nfi.metal.token.Cho;
+import nl.minvenj.nfi.metal.token.Def;
+import nl.minvenj.nfi.metal.token.Nod;
+import nl.minvenj.nfi.metal.token.Opt;
+import nl.minvenj.nfi.metal.token.Pre;
+import nl.minvenj.nfi.metal.token.Rep;
+import nl.minvenj.nfi.metal.token.Seq;
+import nl.minvenj.nfi.metal.token.Str;
+import nl.minvenj.nfi.metal.token.Sub;
+import nl.minvenj.nfi.metal.token.Token;
+import nl.minvenj.nfi.metal.token.While;
 
 @RunWith(Parameterized.class)
 public class ArgumentsTest {
@@ -52,11 +65,12 @@ public class ArgumentsTest {
     final private static ValueExpression VALID_VE = con(1);
     final private static Reducer VALID_REDUCER = new Reducer() { @Override public ValueExpression reduce(final ValueExpression l, final ValueExpression r) { return null; }};
     final private static Expression VALID_E = new Expression() { @Override public boolean eval(final Environment env, final Encoding enc) { return false; }};
+    final private static Token VALID_T = new Token(null) { @Override protected ParseResult parseImpl(final String scope, final Environment env, final Encoding enc) throws IOException { return null; } };
 
     private final Class<?> _class;
     private final Object[] _arguments;
 
-    @Parameters(name = "{0}")
+    @Parameters(name = "{index}-{0}")
     public static Collection<Object[]> arguments() {
         return Arrays.asList(new Object[][] {
             // Derived directly from ValueExpression
@@ -79,6 +93,22 @@ public class ArgumentsTest {
             { Not.class, new Object[] { null } },
             // Derived from ComparisonExpression
             { Eq.class, new Object[] { VALID_VE, null } },
+            // Token implementations
+            { Cho.class, new Object[] { VALID_T, null, null } },
+            { Cho.class, new Object[] { null, VALID_T, null } },
+            { Def.class, new Object[] { VALID_NAME, null, null, null } },
+            { Def.class, new Object[] { null, VALID_VE, null, null } },
+            { Nod.class, new Object[] { null, null } },
+            { Opt.class, new Object[] { null, null } },
+            { Pre.class, new Object[] { null, null, null } },
+            { Rep.class, new Object[] { null, null } },
+            { Seq.class, new Object[] { VALID_T, null, null } },
+            { Seq.class, new Object[] { null, VALID_T, null } },
+            { Str.class, new Object[] { VALID_NAME, null, null, null, null } },
+            { Str.class, new Object[] { null, VALID_T, null, null, null } },
+            { Sub.class, new Object[] { VALID_T, null, null } },
+            { Sub.class, new Object[] { null, VALID_VE, null } },
+            { While.class, new Object[] { null, null, null } }
         });
     }
 
@@ -87,15 +117,17 @@ public class ArgumentsTest {
         _arguments = arguments;
     }
 
-    @Test(expected=IllegalArgumentException.class)
+    @Test
     public void runConstructor() throws Throwable {
         final Constructor<?>[] constructors = _class.getConstructors();
         Assert.assertEquals(1, constructors.length);
         try {
             constructors[0].newInstance(_arguments);
+            Assert.fail("Should have thrown an IllegalArgumentException.");
         }
         catch (final InvocationTargetException e) {
-            throw e.getCause();
+            Assert.assertEquals(IllegalArgumentException.class, e.getCause().getClass());
+            Assert.assertTrue(e.getCause().getMessage().endsWith("may not be null."));
         }
     }
 
