92420964de122ee6980a2d051ca202478ba98a4f
Mattijs Ugen <akaIDIOT@users.noreply.github.com>
2016-02-11 16:12:38 +0100

    METAL-38: Default CurrentOffset to UNSIGNED numbers

--- src/main/java/nl/minvenj/nfi/metal/expression/value/reference/CurrentOffset.java
+++ src/main/java/nl/minvenj/nfi/metal/expression/value/reference/CurrentOffset.java
@@ -26,7 +26,8 @@ public class CurrentOffset implements ValueExpression {
 
     @Override
     public OptionalValue eval(final Environment env, final Encoding enc) {
-        return OptionalValue.of(ConstantFactory.createFromNumeric(env.offset, new Encoding(true)));
+        // override encoding, *always* us unsigned numbers for the current offset
+        return OptionalValue.of(ConstantFactory.createFromNumeric(env.offset, new Encoding(false)));
     }
 
     @Override
