e508a55f2c1528f070843022aac44807cf62bdef
Gertjan Al <gertjan@holmes.nl>
2016-05-26 13:09:37 +0200

    Return zero length when value is null

--- src/main/java/io/parsingdata/metal/expression/value/reference/Len.java
+++ src/main/java/io/parsingdata/metal/expression/value/reference/Len.java
@@ -7,6 +7,7 @@ import io.parsingdata.metal.data.ParseValue;
 import io.parsingdata.metal.encoding.Encoding;
 import io.parsingdata.metal.expression.value.ConstantFactory;
 import io.parsingdata.metal.expression.value.OptionalValue;
+import io.parsingdata.metal.expression.value.Value;
 import io.parsingdata.metal.expression.value.ValueExpression;
 
 public class Len implements ValueExpression {
@@ -20,12 +21,15 @@ public class Len implements ValueExpression {
     @Override
     public OptionalValue eval(final Environment env, final Encoding enc) {
         final ParseValue value = env.order.get(_name);
-        return OptionalValue.of(value == null || value.getValue() == null ? null : ConstantFactory.createFromNumeric(value.getValue().length, new Encoding(true)));
+        return OptionalValue.of(value == null || value.getValue() == null ? num(0) : num(value.getValue().length));
+    }
+
+    private static Value num(final long length) {
+        return ConstantFactory.createFromNumeric(length, new Encoding(true));
     }
 
     @Override
     public String toString() {
         return getClass().getSimpleName() + "(" + _name + ")";
     }
-
 }
