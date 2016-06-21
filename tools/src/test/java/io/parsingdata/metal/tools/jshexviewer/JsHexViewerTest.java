2c9828609e0e86898c1e55c224df951a5d5d7930
Gertjan Al <gertjan@holmes.nl>
2016-06-21 17:02:32 +0200

    Updated license

--- tools/src/test/java/io/parsingdata/metal/tools/jshexviewer/JsHexViewerTest.java
+++ tools/src/test/java/io/parsingdata/metal/tools/jshexviewer/JsHexViewerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2016 Netherlands Forensic Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.parsingdata.metal.tools.jshexviewer;
 
 import static org.junit.Assert.assertEquals;
@@ -19,11 +35,6 @@ import io.parsingdata.metal.data.ParseResult;
 import io.parsingdata.metal.token.Token;
 import io.parsingdata.metal.util.EnvironmentFactory;
 
-/**
- * Generate a HTML page to view the Metal ParseGraph in a hex viewer.
- *
- * @author Netherlands Forensic Institute.
- */
 public class JsHexViewerTest {
 
     private static final Token STRING = seq(
