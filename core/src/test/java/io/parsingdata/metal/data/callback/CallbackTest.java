c2217fcc15faf6bc51fe17cf2ddb175f53055105
Roel van Dijk <dijk@holmes.nl>
2016-10-10 15:31:10 +0200

    #87: Add test with nested callbacks.

--- core/src/test/java/io/parsingdata/metal/data/callback/CallbackTest.java
+++ core/src/test/java/io/parsingdata/metal/data/callback/CallbackTest.java
@@ -25,6 +25,7 @@ import static io.parsingdata.metal.Shorthand.def;
 import static io.parsingdata.metal.Shorthand.eq;
 import static io.parsingdata.metal.Shorthand.rep;
 import static io.parsingdata.metal.Shorthand.seq;
+import static io.parsingdata.metal.data.selection.ByName.getValue;
 import static io.parsingdata.metal.data.selection.ByToken.getAllRoots;
 import static io.parsingdata.metal.util.EncodingFactory.enc;
 import static io.parsingdata.metal.util.TokenDefinitions.any;
@@ -81,10 +82,10 @@ public class CallbackTest {
         assertEquals(1, failureCount);
     }
 
-    private Token simpleSeq = seq(any("a"), any("b"));
+    private static final Token SIMPLE_SEQ = seq(any("a"), any("b"));
 
-    private Callback createCallback(final long... offsets) {
-        return new BaseCallback() {
+    private TokenCallbackList createCallbackList(Token token, final long... offsets) {
+        return TokenCallbackList.create(new TokenCallback(token, new BaseCallback() {
 
             private int count = 0;
 
@@ -96,7 +97,7 @@ public class CallbackTest {
 
             @Override
             protected void handleFailure(Token token, Environment environment) {}
-        };
+        }));
     }
 
     private ParseItemList reverse(final ParseItemList oldList, final ParseItemList newList) {
@@ -106,16 +107,42 @@ public class CallbackTest {
 
     @Test
     public void testSimpleCallback() throws IOException {
-        final TokenCallbackList callbacks = TokenCallbackList.create(new TokenCallback(simpleSeq, createCallback(0L)));
+        final TokenCallbackList callbacks = createCallbackList(SIMPLE_SEQ, 0L);
         final Environment env = new Environment(new InMemoryByteStream(new byte[] { 1, 2 }), callbacks);
-        assertTrue(simpleSeq.parse(env, enc()).succeeded);
+        assertTrue(SIMPLE_SEQ.parse(env, enc()).succeeded);
     }
 
     @Test
     public void testRepSimpleCallback() throws IOException {
-        final TokenCallbackList callbacks = TokenCallbackList.create(new TokenCallback(simpleSeq, createCallback(0L, 2L)));
+        final TokenCallbackList callbacks = createCallbackList(SIMPLE_SEQ, 0L, 2L);
         final Environment env = new Environment(new InMemoryByteStream(new byte[] { 1, 2, 3, 4 }), callbacks);
-        assertTrue(rep(simpleSeq).parse(env, enc()).succeeded);
+        assertTrue(rep(SIMPLE_SEQ).parse(env, enc()).succeeded);
+    }
+
+    @Test
+    public void seqAndRepCallbacks() throws IOException {
+        final Token repeatingSeq = rep(SIMPLE_SEQ);
+        final TokenCallbackList callbacks = createCallbackList(SIMPLE_SEQ, 0L, 2L)
+                .add(new TokenCallback(repeatingSeq, new BaseCallback() {
+                    @Override
+                    protected void handleSuccess(Token token, Environment environment) {
+                        final ParseItemList repRoots = getAllRoots(environment.order, token);
+                        assertEquals(1, repRoots.size);
+
+                        // verify that two Seq tokens were parsed:
+                        final ParseItemList seqRoots = getAllRoots(environment.order, SIMPLE_SEQ);
+                        assertEquals(2, seqRoots.size);
+
+                        // verify order of the two Seq graphs:
+                        assertEquals(2, getValue(seqRoots.head.asGraph(), "a").getOffset());
+                        assertEquals(0, getValue(seqRoots.tail.head.asGraph(), "a").getOffset());
+                    }
+
+                    @Override
+                    protected void handleFailure(Token token, Environment environment) {}
+                }));
+        final Environment env = new Environment(new InMemoryByteStream(new byte[] { 1, 2, 3, 4 }), callbacks);
+        assertTrue(repeatingSeq.parse(env, enc()).succeeded);
     }
 
 }
