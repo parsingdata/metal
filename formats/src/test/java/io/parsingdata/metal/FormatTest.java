9cfd2dea1f908f6aafb8735df46bea667289c77b
Gertjan Al <gertjan@holmes.nl>
2016-10-19 15:36:53 +0200

    Improved uuid tests

--- formats/src/test/java/io/parsingdata/metal/FormatTest.java
+++ formats/src/test/java/io/parsingdata/metal/FormatTest.java
@@ -29,7 +29,9 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -45,6 +47,9 @@ public class FormatTest {
     private static final String ZIPFILE1 = "/singlefile-zip30-ubuntu.zip";
     private static final String ZIPFILE2 = "/multifile-zip30-ubuntu.zip";
     private static final String JPEGFILE = "/test.jpg";
+
+    @Rule
+    public ExpectedException _exception = ExpectedException.none();
 
     @Test
     public void parsePNG() throws IOException, URISyntaxException {
@@ -71,11 +76,15 @@ public class FormatTest {
         final Token guid = def("guid", 16, guid("2dc27766-f623-4200-9d64-115e9bfd4a08"));
         assertFalse(guid.parse(stream(0x2d, 0xc2, 0x77, 0x66, 0xf6, 0x23, 0x42, 0x00, 0x9d, 0x64, 0x11, 0x5e, 0x9b, 0xfd, 0x4a, 0x08), enc()).succeeded);
         assertTrue(guid.parse(stream(0x66, 0x77, 0xc2, 0x2d, 0x23, 0xf6, 0x00, 0x42, 0x9d, 0x64, 0x11, 0x5e, 0x9b, 0xfd, 0x4a, 0x08), enc()).succeeded);
+
+        _exception.expect(IllegalArgumentException.class);
+        _exception.expectMessage("Invalid GUID string: test");
+        guid("test");
     }
 
     @Test
     public void parseUUID() throws IOException, URISyntaxException {
-        final Token uuid = def("guid", 16, uuid("2dc27766-f623-4200-9d64-115e9bfd4a08"));
+        final Token uuid = def("uuid", 16, uuid("2dc27766-f623-4200-9d64-115e9bfd4a08"));
         assertTrue(uuid.parse(stream(0x2d, 0xc2, 0x77, 0x66, 0xf6, 0x23, 0x42, 0x00, 0x9d, 0x64, 0x11, 0x5e, 0x9b, 0xfd, 0x4a, 0x08), enc()).succeeded);
         assertFalse(uuid.parse(stream(0x66, 0x77, 0xc2, 0x2d, 0x23, 0xf6, 0x00, 0x42, 0x9d, 0x64, 0x11, 0x5e, 0x9b, 0xfd, 0x4a, 0x08), enc()).succeeded);
     }
