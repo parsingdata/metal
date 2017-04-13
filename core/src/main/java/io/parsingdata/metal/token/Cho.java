df4249f504e03b9923d760faaf6e9bc43deff476
Jeroen van den Bos <jeroen@infuse.org>
2017-04-13 09:12:47 +0200

    #162: Reimplemented Cho with trampoline.

--- core/src/main/java/io/parsingdata/metal/token/Cho.java
+++ core/src/main/java/io/parsingdata/metal/token/Cho.java
@@ -25,9 +25,13 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
 
+import io.parsingdata.metal.Util;
 import io.parsingdata.metal.data.Environment;
 import io.parsingdata.metal.data.ImmutableList;
 import io.parsingdata.metal.encoding.Encoding;
+import io.parsingdata.metal.token.util.FinalTrampoline;
+import io.parsingdata.metal.token.util.IntermediateTrampoline;
+import io.parsingdata.metal.token.util.Trampoline;
 
 /**
  * A {@link Token} that specifies a choice out of a list of tokens.
@@ -48,20 +52,23 @@ public class Cho extends Token {
 
     @Override
     protected Optional<Environment> parseImpl(final String scope, final Environment environment, final Encoding encoding) throws IOException {
-        final Optional<Environment> result = iterate(scope, environment.addBranch(this), encoding, tokens);
+        final Optional<Environment> result = iterate(scope, Optional.of(environment.addBranch(this)), encoding, tokens).computeResult();
         if (result.isPresent()) {
             return success(result.get().closeBranch());
         }
         return failure();
     }
 
-    private Optional<Environment> iterate(final String scope, final Environment environment, final Encoding encoding, final ImmutableList<Token> list) throws IOException {
+    private Trampoline<Optional<Environment>> iterate(final String scope, final Optional<Environment> environment, final Encoding encoding, final ImmutableList<Token> list) throws IOException {
         if (list.isEmpty()) {
-            return failure();
+            return (FinalTrampoline<Optional<Environment>>) Util::failure;
         }
-        final Optional<Environment> result = list.head.parse(scope, environment, encoding);
-        if (result.isPresent()) { return result; }
-        return iterate(scope, environment, encoding, list.tail);
+        final Optional<Environment> result = list.head.parse(scope, environment.get(), encoding);
+        if (result.isPresent()) {
+            return (FinalTrampoline<Optional<Environment>>) () -> success(result.get());
+        } else {
+            return (IntermediateTrampoline<Optional<Environment>>) () -> iterate(scope, environment, encoding, list.tail);
+        }
     }
 
     @Override
