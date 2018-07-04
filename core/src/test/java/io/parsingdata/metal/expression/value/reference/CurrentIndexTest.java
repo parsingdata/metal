dac4afb17a4281587ad3d5b9df977c49b84a4197
Jeroen van den Bos <jeroen@infuse.org>
2018-07-04 22:03:36 +0200

    #236: Added (failing) test for using CurrentIndex in a nested iterable.

--- core/src/test/java/io/parsingdata/metal/expression/value/reference/CurrentIndexTest.java
+++ core/src/test/java/io/parsingdata/metal/expression/value/reference/CurrentIndexTest.java
@@ -44,8 +44,9 @@ public class CurrentIndexTest extends ParameterizedParse {
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
             { "[0, 1, 2, 3, 255] rep(CURRENT_INDEX), def(255)", seq(rep(VALUE_EQ_INDEX), def("value", con(1), eq(con(255)))), stream(0, 1, 2, 3, 255), enc(), true },
-            { "[0, 1, 2, 3] repn(4, CURRENT_INDEX)", repn(VALUE_EQ_INDEX, con(4)), stream(0, 1, 2, 3), enc(), true },
-            { "[255, 0, 1, 2, 3] def(255), while(<3, CURRENT_INDEX)", seq(def("value", con(1), eq(con(255))), whl(VALUE_EQ_INDEX, ltNum(con(3)))), stream(255, 0, 1, 2, 3), enc(), true },
+            { "[0, 1, 2, 3] repn=3(CURRENT_INDEX)", repn(VALUE_EQ_INDEX, con(4)), stream(0, 1, 2, 3), enc(), true },
+            { "[255, 0, 1, 2, 3] def(255), while<3(CURRENT_INDEX)", seq(def("value", con(1), eq(con(255))), whl(VALUE_EQ_INDEX, ltNum(con(3)))), stream(255, 0, 1, 2, 3), enc(), true },
+            { "[0, 0, 1, 2, 1, 0, 1, 2] repn=2(CURRENT_INDEX, repn=3(CURRENT_INDEX))", repn(seq(VALUE_EQ_INDEX, repn(VALUE_EQ_INDEX, con(3))), con(2)), stream(0, 0, 1, 2, 1, 0, 1, 2), enc(), true },
             //{ "[0, 1, 2, 3] seq(CURRENT_INDEX, ...)", seq(VALUE_EQ_INDEX, VALUE_EQ_INDEX, VALUE_EQ_INDEX, VALUE_EQ_INDEX), stream(0, 1, 2, 3), enc(), true }
         });
     }
