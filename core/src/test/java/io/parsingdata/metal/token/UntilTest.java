c3e1593a0053d3ad1feebd0137da36c8447725e9
Marja van Aken <marja@infix.ai>
2023-03-01 14:52:12 +0100

    (#360) Renamed defU shorthand to def and reimplemented until using seq.

--- core/src/test/java/io/parsingdata/metal/token/UntilTest.java
+++ core/src/test/java/io/parsingdata/metal/token/UntilTest.java
@@ -20,7 +20,7 @@ import static io.parsingdata.metal.Shorthand.CURRENT_OFFSET;
 import static io.parsingdata.metal.Shorthand.EMPTY;
 import static io.parsingdata.metal.Shorthand.con;
 import static io.parsingdata.metal.Shorthand.def;
-import static io.parsingdata.metal.Shorthand.defU;
+import static io.parsingdata.metal.Shorthand.def;
 import static io.parsingdata.metal.Shorthand.eq;
 import static io.parsingdata.metal.Shorthand.last;
 import static io.parsingdata.metal.Shorthand.mod;
@@ -47,10 +47,10 @@ import io.parsingdata.metal.data.ImmutableList;
 import io.parsingdata.metal.data.ParseState;
 import io.parsingdata.metal.data.ParseValue;
 import io.parsingdata.metal.encoding.Encoding;
+import io.parsingdata.metal.expression.Expression;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.runners.Parameterized.Parameters;
 
 class UntilTest {
 
@@ -59,12 +59,12 @@ class UntilTest {
     private static final String INPUT_3 = "Another way to scroll...";
     private static final String INPUT = INPUT_1 + "\n" + INPUT_2 + "\n" + INPUT_3 + "\n";
 
+    public static final Expression ENDS_WITH_NEWLINE = eq(mod(last(ref("line")), con(256)), con('\n'));
     public static final Token NEWLINE = def("newline", con(1), eq(con('\n')));
-    public static final Token END_WITH_NEWLINE_POST = post(EMPTY, eq(mod(last(ref("line")), con(256)), con('\n')));
+    public static final Token END_WITH_NEWLINE_POST = post(EMPTY, ENDS_WITH_NEWLINE);
     public static final Token END_WITH_NEWLINE_SUB = sub(NEWLINE, sub(CURRENT_OFFSET, con(1)));
     public static final Token NEXT_START_WITH_TERMINATOR = sub(NEWLINE, CURRENT_OFFSET);
 
-    @Parameters(name="{0}")
     static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
             { "until: terminator not part of line, available in parseGraph",  until("line", NEWLINE),                                  3, 3, INPUT_1, INPUT_2, INPUT_3},
@@ -72,16 +72,17 @@ class UntilTest {
             { "until: terminator part of line, available in parseGraph",      until("line", con(1), END_WITH_NEWLINE_SUB),       3, 3, INPUT_1 + '\n', INPUT_2 + '\n', INPUT_3 + '\n'},
             { "until: terminator part of next line, available in parseGraph", until("line", con(1), NEXT_START_WITH_TERMINATOR), 3, 3, INPUT_1, '\n' + INPUT_2, '\n' + INPUT_3},
 
-            { "defU: terminator not part of line, available in parseGraph",  seq(defU("line", NEWLINE), NEWLINE),                        3, 3, INPUT_1, INPUT_2, INPUT_3},
-            { "defU: terminator part of line, not available in parseGraph",      defU("line", con(1), END_WITH_NEWLINE_POST),      3, 0, INPUT_1 + '\n', INPUT_2 + '\n', INPUT_3 + '\n'},
-            { "defU: terminator part of line, not available in parseGraph",      defU("line", con(1), END_WITH_NEWLINE_SUB),       3, 0, INPUT_1 + '\n', INPUT_2 + '\n', INPUT_3 + '\n'},
-            { "defU: terminator part of next line, not available in parseGraph", defU("line", con(1), NEWLINE),                    3, 0, INPUT_1, '\n' + INPUT_2, '\n' + INPUT_3},
-            { "defU: terminator part of next line, not available in parseGraph", defU("line", con(1), NEXT_START_WITH_TERMINATOR), 3, 0, INPUT_1, '\n' + INPUT_2, '\n' + INPUT_3},
-            // "defU: terminator part of line, available in parseGraph" is not possible with defU, only with until.
+            { "def: terminator not part of line, available in parseGraph",  seq(def("line", NEWLINE), NEWLINE),                        3, 3, INPUT_1, INPUT_2, INPUT_3},
+            { "def: terminator part of line, not available in parseGraph",      def("line", con(1), END_WITH_NEWLINE_POST),      3, 0, INPUT_1 + '\n', INPUT_2 + '\n', INPUT_3 + '\n'},
+            { "def: terminator part of line, not available in parseGraph",      def("line", con(1), END_WITH_NEWLINE_SUB),       3, 0, INPUT_1 + '\n', INPUT_2 + '\n', INPUT_3 + '\n'},
+            { "def: terminator part of line, not available in parseGraph",      def("line", ENDS_WITH_NEWLINE),                        3, 0, INPUT_1 + '\n', INPUT_2 + '\n', INPUT_3 + '\n'},
+            { "def: terminator part of next line, not available in parseGraph", def("line", con(1), NEWLINE),                    3, 0, INPUT_1, '\n' + INPUT_2, '\n' + INPUT_3},
+            { "def: terminator part of next line, not available in parseGraph", def("line", con(1), NEXT_START_WITH_TERMINATOR), 3, 0, INPUT_1, '\n' + INPUT_2, '\n' + INPUT_3},
+            // "def: terminator part of line, available in parseGraph" is not possible with def, only with until.
         });
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name="{0}")
     @MethodSource("data")
     void repTest(final String name, final Token token, final int lineCount, final int newlineCount, final String line1, final String line2, final String line3) {
         final Optional<ParseState> parseState = rep(token).parse(env(stream(INPUT, US_ASCII)));
@@ -97,22 +98,21 @@ class UntilTest {
         assertEquals(newlineCount, newLines.size);
     }
 
-    @Parameters(name="{0}")
-    static Collection<Object[]> defUShorthands() {
+    static Collection<Object[]> shorthandTokenTest() {
         return Arrays.asList(new Object[][] {
-            { "defU",                         defU("line", NEWLINE)               },
-            { "defU initial size",            defU("line", con(13), NEWLINE)},
-            { "defU initial size + encoding", defU("line", con(13), NEWLINE, Encoding.DEFAULT_ENCODING)},
-            { "defU step size",               defU("line", con(13), con(1), NEWLINE)},
-            { "defU step size + encoding",    defU("line", con(13), con(1), NEWLINE, Encoding.DEFAULT_ENCODING)},
-            { "defU max size",                defU("line", con(13), con(1), con(24), NEWLINE)},
-            { "defU max size + encoding",     defU("line", con(13), con(1), con(24), NEWLINE, Encoding.DEFAULT_ENCODING)},
+            { "def",                         def("line", NEWLINE)               },
+            { "def initial size",            def("line", con(13), NEWLINE)},
+            { "def initial size + encoding", def("line", con(13), NEWLINE, Encoding.DEFAULT_ENCODING)},
+            { "def step size",               def("line", con(13), con(1), NEWLINE)},
+            { "def step size + encoding",    def("line", con(13), con(1), NEWLINE, Encoding.DEFAULT_ENCODING)},
+            { "def max size",                def("line", con(13), con(1), con(24), NEWLINE)},
+            { "def max size + encoding",     def("line", con(13), con(1), con(24), NEWLINE, Encoding.DEFAULT_ENCODING)},
         });
     }
 
-    @ParameterizedTest
-    @MethodSource("defUShorthands")
-    void shorthandTest(final String name, final Token token) {
+    @ParameterizedTest(name="{0}")
+    @MethodSource
+    void shorthandTokenTest(final String name, final Token token) {
         final Optional<ParseState> parseState = rep(seq(token, NEWLINE)).parse(env(stream(INPUT, US_ASCII)));
         assertTrue(parseState.isPresent());
 
@@ -124,6 +124,30 @@ class UntilTest {
 
         ImmutableList<ParseValue> newLines = getAllValues(parseState.get().order, "newline");
         assertEquals(3, newLines.size);
+    }
+
+    static Collection<Object[]> shorthandExpressionTest() {
+        return Arrays.asList(new Object[][] {
+            { "def token",                   def("line", con(1), END_WITH_NEWLINE_POST)},
+            { "def expression",              def("line", ENDS_WITH_NEWLINE)},
+            { "def expression + encoding",   def("line", ENDS_WITH_NEWLINE, Encoding.DEFAULT_ENCODING)},
+        });
+    }
+
+    @ParameterizedTest(name="{0}")
+    @MethodSource
+    void shorthandExpressionTest(final String name, final Token token) {
+        final Optional<ParseState> parseState = rep(token).parse(env(stream(INPUT, US_ASCII)));
+        assertTrue(parseState.isPresent());
+
+        ImmutableList<ParseValue> values = getAllValues(parseState.get().order, "line");
+        assertEquals(3, values.size);
+        assertEquals(INPUT_1 + "\n", values.tail.tail.head.asString());
+        assertEquals(INPUT_2 + "\n", values.tail.head.asString());
+        assertEquals(INPUT_3 + "\n", values.head.asString());
+
+        ImmutableList<ParseValue> newLines = getAllValues(parseState.get().order, "newline");
+        assertEquals(0, newLines.size);
     }
 
     @Test
