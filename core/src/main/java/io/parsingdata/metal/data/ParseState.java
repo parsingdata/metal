5843a06b10ad03caa4b4ac0cafea592fc5d8210d
Marja van Aken <marja@infix.ai>
2023-11-02 21:52:18 +0100

    #399: Cache is never null, so no need to check for it in toString.

--- core/src/main/java/io/parsingdata/metal/data/ParseState.java
+++ core/src/main/java/io/parsingdata/metal/data/ParseState.java
@@ -105,8 +105,7 @@ public class ParseState {
     public String toString() {
         final String iterationsString = iterations.isEmpty() ? "" : ";iterations:" + iterations;
         final String referencesString = references.isEmpty() ? "" : ";references:" + references;
-        final String cacheString = cache == null ? "" : ";" + cache;
-        return getClass().getSimpleName() + "(source:" + source + ";offset:" + offset + ";order:" + order + iterationsString + referencesString + cacheString + ")";
+        return getClass().getSimpleName() + "(source:" + source + ";offset:" + offset + ";order:" + order + iterationsString + referencesString + ";" + cache + ")";
     }
 
     @Override
