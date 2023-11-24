b0b6fca733aefa6cac4c78d83d8585d8bb80d94e
Marja van Aken <marja@infix.ai>
2023-11-24 11:24:17 +0100

    #402: Update core/src/main/java/io/parsingdata/metal/ImmutableObject.java
    Minor typo.
    
    Co-authored-by: Carly-1 <75033199+Carly-1@users.noreply.github.com>

--- core/src/main/java/io/parsingdata/metal/ImmutableObject.java
+++ core/src/main/java/io/parsingdata/metal/ImmutableObject.java
@@ -2,7 +2,7 @@ package io.parsingdata.metal;
 
 /**
  * When objects are immutable, that means their hashcode will stay the same the moment it is created.
- * It will improve performance if these objects caches their hash.
+ * It will improve performance if these objects cache their hash.
  * <p>
  * This is a lazy implementation, instead of calculating the hash once within the constructor, to avoid
  * performance decrease during parsing. In most implementations the hash is not actually used/needed.
